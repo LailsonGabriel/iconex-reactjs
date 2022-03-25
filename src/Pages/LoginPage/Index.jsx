afd24f4595a2688eee144d92e9e9dbb8e95c8b9c
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2022-03-25 10:43:18 -0300

    fix: Login page handle inputs

--- src/Pages/LoginPage/Index.jsx
+++ src/Pages/LoginPage/Index.jsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
 import InputReusable from "../../Components/Input/Index";
+import setInputState from "../../utils/setInputState";
 
 function LoginPage() {
   const [loginInfos, setLoginInfos] = useState({ email: "", password: "" });
 
+  const handleLogin = (e) => {
+    setInputState(e, loginInfos, setLoginInfos);
+  };
+
   return (
     <div>
-      <InputReusable placeholder='Email' />
-      <InputReusable placeholder='Senha' />
+      <InputReusable
+        placeholder='Email'
+        onChange={handleLogin}
+        name='email'
+        value={loginInfos.email}
+      />
+      <InputReusable
+        placeholder='Senha'
+        onChange={handleLogin}
+        name='password'
+        value={loginInfos.password}
+      />
     </div>
   );
 }
